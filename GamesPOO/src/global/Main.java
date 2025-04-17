2f3de65414ac3785d9bfa372dafae3bcf650d4ad
Francisco-Simoes-Oliveira <simoesdeoliveirafrancisco@gmail.com>
2025-04-16 21:55:40 -0300

    Update Main.java

--- GamesPOO/src/global/Main.java
+++ GamesPOO/src/global/Main.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner sc = new Scanner(System.in);
-    public static ControlJogo controlJogo = new ControlJogo(2);
+    public static ControlJogo controlJogo = new ControlJogo(1);
     public static Random random = new Random();
     public static Palavras[] palavras = new Palavras[controlJogo.quatidadeDePalavras];
     public static GerenciamentoDeDicas dicas = new GerenciamentoDeDicas();
@@ -32,6 +32,12 @@ public class Main {
         } else {
             System.out.println("Numero escolhido invalido");
         }
+
+    }
+    public static void reset(){
+        sc.nextLine();
+        controlJogo = new ControlJogo(controlJogo.quatidadeDePalavras, controlJogo.pontos);
+        dicas = new GerenciamentoDeDicas();
 
     }
 }
