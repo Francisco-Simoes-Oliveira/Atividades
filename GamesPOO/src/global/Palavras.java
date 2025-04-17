a4d3d9079f094f40ed5daf2557b6d758c7f46216
Francisco-Simoes-Oliveira <simoesdeoliveirafrancisco@gmail.com>
2025-04-16 22:17:33 -0300

    Update Palavras.java

--- GamesPOO/src/global/Palavras.java
+++ GamesPOO/src/global/Palavras.java
@@ -9,6 +9,14 @@ public class Palavras {
         String opcoesPavras[] = {"formol","acervo","vender","vaso","ator","bobo","livro","carta","garfo","dor","sal"};
         for (int x=0;x<Main.controlJogo.quatidadeDePalavras;x++){
             Main.palavras[x].palavra = opcoesPavras[Main.random.nextInt(opcoesPavras.length)];
+            if (x>0){
+                for (int y=0; y < x; y++) {
+                    if (Main.palavras[x].palavra.equals(Main.palavras[y].palavra)){
+                        x -= 1;
+                        break;
+                    }
+                }
+            }
         }
     }
 
