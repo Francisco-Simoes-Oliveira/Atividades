6823e7e26211f04a70a6c670e22884505f3b159a
Francisco-Simoes-Oliveira <simoesdeoliveirafrancisco@gmail.com>
2025-05-28 21:53:36 -0300

    Update BubbleSort.java

--- Algoritmos e Estrutura de Dados II/aps1005_Algoritimo/src/ordenacao/BubbleSort.java
+++ Algoritmos e Estrutura de Dados II/aps1005_Algoritimo/src/ordenacao/BubbleSort.java
@@ -1,157 +1,198 @@
 package ordenacao;
-import global.Paciente;
 
-public class BubbleSort {
+import global.Imprimir;
 
-    //Int
-    public static void ordenacaoCrescente(int[] vetor)/*Ordenar Int de forma Crescente*/{
-        int aux;
-        boolean troca;
-        for (int x=0;x<vetor.length;x++){
-            troca = false;
-            for (int y=0;y<vetor.length -(x+1);y++){
-                if (vetor[y] > vetor[y+1]){
-                    aux = vetor[y];
-                    vetor[y] = vetor[y+1];
-                    vetor[y+1] = aux;
+public class MergeSort {
 
-                    troca = true;
-                }
-            }
-            if (!troca) break;
+    public static int quandidade = 0;
+
+    public static void ordenacao(int vetor[]){
+        int aux[] = new int[vetor.length];
+        ordenacaoMarge(vetor, aux, 0, vetor.length - 1,1);
+    }
+    public static void ordenacao(int tipo ,int vetor[]){
+        int aux[] = new int[vetor.length];
+        ordenacaoMarge(vetor, aux, 0, vetor.length - 1,tipo);
+    }
+
+    private static void ordenacaoMarge(int vetor[],int aux[], int inicio, int fim,int tipo){
+        int meio = (inicio + fim)/2;
+
+        if (inicio<fim){
+            ordenacaoMarge(vetor,aux,inicio,meio,tipo);
+            ordenacaoMarge(vetor,aux,meio+1,fim,tipo);
+            if (tipo == 1)intercalacaoCrecente(vetor, aux, inicio, meio, fim);
+            else if (tipo == -1)intercalacaoDecrecente(vetor, aux, inicio, meio, fim);
         }
     }
 
-    public static void ordenacaoCrescente(int[] vetor, int n)/*Ordenar Int de forma Crescente, Ate n*/{
-        int aux;
-        boolean troca;
-        for (int x=0;x<n;x++){
-            troca = false;
-            for (int y=0;y<n -(x+1);y++){
-                if (vetor[y] > vetor[y+1]){
-                    aux = vetor[y];
-                    vetor[y] = vetor[y+1];
-                    vetor[y+1] = aux;
+    public static void intercalacaoCrecente(int vetor[], int aux[], int inicio, int meio, int fim){
 
-                    troca = true;
-                }
+        for (int x=0; x<= fim;x++){
+            aux[x] =vetor[x];
+        }
+
+        int esquerda = inicio;
+        int direita = meio+1;
+
+        for (int x=inicio; x <= fim;x++){
+            if(esquerda>meio) {
+                vetor[x] = aux[direita++];
+            }else if(direita>fim) {
+                vetor[x] = aux[esquerda++];
+            }else if(aux[esquerda] < aux[direita]) {
+                vetor[x] = aux[esquerda++];
+            }else {
+                vetor[x] = aux[direita++];
             }
-            if (!troca) break;
+        }
+    }
+    public static void intercalacaoDecrecente(int vetor[], int aux[], int inicio, int meio, int fim){
+
+        for (int x=0; x<= fim;x++){
+            aux[x] =vetor[x];
+        }
+
+        int esquerda = inicio;
+        int direita = meio+1;
+
+        for (int x=inicio; x <= fim;x++){
+            if(esquerda>meio) {
+                vetor[x] = aux[direita++];
+            }else if(direita>fim) {
+                vetor[x] = aux[esquerda++];
+            }else if(aux[esquerda] > aux[direita]) {
+                vetor[x] = aux[esquerda++];
+            }else {
+                vetor[x] = aux[direita++];
+            }
         }
     }
 
-    public static void ordenacaoDecrescente(int[] vetor)/*Ordenar Int de forma Decrescente*/{
-        int aux;
-        boolean troca;
-        for (int x=0;x<vetor.length;x++){
-            troca = false;
-            for (int y=0;y<vetor.length -(x+1);y++){
-                if (vetor[y] < vetor[y+1]){
-                    aux = vetor[y];
-                    vetor[y] = vetor[y+1];
-                    vetor[y+1] = aux;
 
-                    troca = true;
-                }
-            }
-            if (!troca) break;
-        }
+
+    //---------------String----------------------------//
+
+    public static void ordenacao(String vetor[]){
+        String aux[] = new String[vetor.length];
+        ordenacaoMarge(vetor, aux, 0, vetor.length - 1,1);
+    }
+    public static void ordenacao(int tipo ,String vetor[]){
+        String aux[] = new String[vetor.length];
+        ordenacaoMarge(vetor, aux, 0, vetor.length - 1,tipo);
     }
 
 
-    //Strings
-    public static void ordenacaoCrescente(String[] vetor)/*Ordenar String de forma Crescente*/{
-        String aux;
-        boolean troca;
-        for (int x=0;x<vetor.length;x++){
-            troca = false;
-            for (int y=0;y<vetor.length -(x+1);y++){
-                if (vetor[y].compareTo(vetor[y+1]) > 0){
-                    aux = vetor[y];
-                    vetor[y] = vetor[y+1];
-                    vetor[y+1] = aux;
+    private static void ordenacaoMarge(String vetor[],String aux[], int inicio, int fim,int tipo){
+        int meio = (inicio + fim)/2;
 
-                    troca = true;
-                }
+        if (inicio<fim){
+            ordenacaoMarge(vetor,aux,inicio,meio,tipo);
+            ordenacaoMarge(vetor,aux,meio+1,fim,tipo);
+            if (tipo == 1)intercalacaoCrecente(vetor, aux, inicio, meio, fim);
+            else if (tipo == -1)intercalacaoDecrecente(vetor, aux, inicio, meio, fim);
+        }
+    }
+
+    public static void intercalacaoCrecente(String vetor[], String aux[], int inicio, int meio, int fim){
+
+        for (int x=0; x<= fim;x++){
+            aux[x] =vetor[x];
+        }
+
+        int esquerda = inicio;
+        int direita = meio+1;
+
+        for (int x=inicio; x <= fim;x++){
+            if(esquerda>meio) {
+                vetor[x] = aux[direita++];
+            }else if(direita>fim) {
+                vetor[x] = aux[esquerda++];
+            }else if(aux[esquerda].compareTo(aux[direita]) < 0) {
+                vetor[x] = aux[esquerda++];
+            }else {
+                vetor[x] = aux[direita++];
             }
-            if (!troca) break;
+        }
+    }
+    public static void intercalacaoDecrecente(String vetor[], String aux[], int inicio, int meio, int fim){
+
+        for (int x=0; x<= fim;x++){
+            aux[x] =vetor[x];
+        }
+
+        int esquerda = inicio;
+        int direita = meio+1;
+
+        for (int x=inicio; x <= fim;x++){
+            if(esquerda>meio) {
+                vetor[x] = aux[direita++];
+            }else if(direita>fim) {
+                vetor[x] = aux[esquerda++];
+            }else if(aux[esquerda].compareTo(aux[direita]) > 0) {
+                vetor[x] = aux[esquerda++];
+            }else {
+                vetor[x] = aux[direita++];
+            }
         }
     }
 
 
     //Especiais
 
-    public static void ordenarMatriz(int[][] matriz)/*Matriz*/{
-        for(int x=0; x< matriz.length;x++){
-            ordenacaoCrescente(matriz[x]);
-        }
-    }
-
-    public static void ordenarPaciente(Paciente paciente[])/*Pacientes*/{
-        Paciente aux;
-        boolean troca;
-        for (int x=0;x<paciente.length;x++){
-            troca = false;
-            for (int y=0;y<paciente.length -(x+1);y++){
-                if (paciente[y].gravidade < paciente[y+1].gravidade){
-                    aux = paciente[y];
-                    paciente[y] = paciente[y+1];
-                    paciente[y+1] = aux;
-
-                    troca = true;
-                } else if (paciente[y].gravidade == paciente[y+1].gravidade){
-                    if(paciente[y].tempo < paciente[y+1].tempo){
-                        aux = paciente[y];
-                        paciente[y] = paciente[y+1];
-                        paciente[y+1] = aux;
-
-                        troca = true;
-                    }
-                }
-            }
-            if (!troca) break;
-        }
-    }
-
-    public static long ordenacaoCrescenteTempo(int[] vetor)/*Medir tempo*/{
+    public static long ordenacaoTempo(int vetor[]){
         long inicio = System.nanoTime();
-        int aux;
-        boolean troca;
-        for (int x=0;x<vetor.length;x++){
-            troca = false;
-            for (int y=0;y<vetor.length -(x+1);y++){
-                if (vetor[y] > vetor[y+1]){
-                    aux = vetor[y];
-                    vetor[y] = vetor[y+1];
-                    vetor[y+1] = aux;
-
-                    troca = true;
-                }
-            }
-            if (!troca) break;
-        }
+        int aux[] = new int[vetor.length];
+        ordenacaoMarge(vetor, aux, 0, vetor.length - 1,1);
         long fim = System.nanoTime();
         return fim - inicio;
     }
-    public static int ordenacaoCrescenteQuantia(int[] vetor)/*Medir tempo e comtar operação*/{
-        int quandidade = 0;
-        int aux;
-        boolean troca;
-        for (int x=0;x<vetor.length;x++){
-            troca = false;
-            for (int y=0;y<vetor.length -(x+1);y++){
-                if (vetor[y] > vetor[y+1]){
-                    aux = vetor[y];
-                    vetor[y] = vetor[y+1];
-                    vetor[y+1] = aux;
-                    quandidade++;
-                    troca = true;
-                }
-            }
-            if (!troca) break;
+
+    public static int ordenacaoQuantia(int vetor[]){
+        quandidade = 0;
+        int aux[] = new int[vetor.length];
+        ordenacaoMarge(vetor, aux, 0, vetor.length - 1,1, 0);
+        Imprimir.printVetor(vetor);
+        return quandidade;
+    }
+
+    private static void ordenacaoMarge(int vetor[],int aux[], int inicio, int fim,int tipo, int n){
+        int meio = (inicio + fim)/2;
+
+        if (inicio<fim){
+            ordenacaoMarge(vetor,aux,inicio,meio,tipo);
+            ordenacaoMarge(vetor,aux,meio+1,fim,tipo);
+            if (tipo == 1)intercalacaoCrecente(vetor, aux, inicio, meio, fim, n);
+            //else if (tipo == -1)intercalacaoDecrecente(vetor, aux, inicio, meio, fim, quandidade);
+        }
+    }
+
+    public static void intercalacaoCrecente(int vetor[], int aux[], int inicio, int meio, int fim, int n){
+
+        for (int x=0; x<= fim;x++){
+            aux[x] =vetor[x];
         }
 
-        return quandidade;
+        int esquerda = inicio;
+        int direita = meio+1;
+
+        for (int x=inicio; x <= fim;x++){
+            if(esquerda>meio) {
+                vetor[x] = aux[direita++];
+                quandidade++;
+            }else if(direita>fim) {
+                vetor[x] = aux[esquerda++];
+                quandidade++; // Onde voce colocou para contar o merge
+            }else if(aux[esquerda] < aux[direita]) {
+                vetor[x] = aux[esquerda++];
+                quandidade++;
+            }else if(aux[esquerda] > aux[direita]){
+                vetor[x] = aux[direita++];
+                quandidade++;
+            }
+
+        }
+
     }
 
 }
